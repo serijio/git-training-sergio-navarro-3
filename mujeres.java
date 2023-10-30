40f9341e663f40af93c19a6069913d5bab4a5fa5
Serijio <serijiomainsett@gmail.com>
2023-10-30 13:23:53 +0100

    primera modi

---
+++ mujeres.java
@@ -0,0 +1,17 @@
+public static public class mujeres {
+
+	public static void main(String[] args) {
+
+	int num1 = 1;
+	int num2 = 2;
+	int num3 = 3;
+
+	String primer = "Gwendoline Christie";
+	String segun = "Cate Blanchett";
+	String tercer = "Margot Robbie";
+
+	System.out.println("El orden de increibles feminas es:\n" + num1 + ": " + primer + "\n"
+				+ num2 + ": " + segun "\n"
+				+ num3 + ": " + tercer);
+	}
+}
